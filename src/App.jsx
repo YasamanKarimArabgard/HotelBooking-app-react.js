4d3a5eba13ca518ec1690259cef04c6b6131c7f7
jasminearabgard <yasaman.arabgard@outlook.com>
2023-09-27 15:56:27 +0330

    import header comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header/Header"
+
 function App() {
   return (
-    <>
-      <h1>Hello Jasmine</h1>
-    </>
+    <main className="bg-gray-50 min-h-screen max-w-screen-2xl max-w-screen grid grid-cols-12">
+      <Header />
+    </main>
   )
 }
 
