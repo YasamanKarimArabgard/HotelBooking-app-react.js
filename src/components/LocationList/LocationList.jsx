81cc4ecb378df69f82c15df624eee4004cab178c
jasminearabgard <yasaman.arabgard@outlook.com>
2023-10-01 21:37:18 +0330

    fetch hotels and filter on it

--- src/components/LocationList/LocationList.jsx
+++ src/components/LocationList/LocationList.jsx
@@ -5,16 +5,28 @@ const LocationList = () => {
 
     const { loading, data } = useFetch('http://localhost:5000/hotels');
 
-    if(loading) return <p>loading...</p>
-
     return (
-        <div className='locationList-container'>
-            <h2>Nearby Loactions</h2>
-            <div>
+        <div className='locationList-container flex flex-col row-start-2 col-start-2 col-span-10 bg-white rounded-lg border p-4 gap-y-3'>
+            <h2 className='font-bold lg:text-2xl'>Nearby Loactions</h2>
+            <div className='locationList grid md:grid-cols-3 lg:grid-cols-4 gap-y-2 gap-x-3'>
                 {
-                    data.map(item => (
-                        <h3>{item.name}</h3>
-                    ))
+                    loading ? <p>loading...</p> :
+                        data.map(item => (
+                            <div className='locationList_item flex flex-col justify-between h-60 rounded-sm overflow-hidden'>
+                                {/* item img */}
+                                <div className='ocationList_item-img h-40 max-h-40'>
+                                    <img src={item.picture_url.url} className='w-full h-full rounded-br-xl rounded-bl-xl' alt={item.name}/>
+                                </div>
+                                <div className='h-1/3 flex flex-col gap-y-1 p-1 justify-between'>
+                                    <h2 className='font-bold text-sm'>{item.smart_location}</h2>
+                                    <h3 className='text-xs text-slate-600'>{item.name}</h3>
+                                    <div className='flex gap-x-1 items-center'> 
+                                       <h5 className='text-xs'>€ {item.price}</h5>
+                                       <span className='text-xs text-slate-500'>night</span>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
                 }
             </div>
         </div>
