be1b48e8c700b7b1ba5bfcb8aa17e31989038fc4
jasminearabgard <yasaman.arabgard@outlook.com>
2023-10-01 19:29:19 +0330

    add loactionList comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
+import LocationList from './components/LocationList/LocationList'
 
 function App() {
   return (
     <main className="bg-gray-50 min-h-screen max-w-screen-2xl max-w-screen grid grid-cols-12">
       <Header />
+      <LocationList />
     </main>
   )
 }
