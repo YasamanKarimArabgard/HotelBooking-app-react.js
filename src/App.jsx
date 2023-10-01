37cdc37460f451fdd123c7275064c9df87feebe9
jasminearabgard <yasaman.arabgard@outlook.com>
2023-10-01 21:36:40 +0330

    add rows grid

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import LocationList from './components/LocationList/LocationList'
 
 function App() {
   return (
-    <main className="bg-gray-50 min-h-screen max-w-screen-2xl max-w-screen grid grid-cols-12">
+    <main className="bg-gray-50 min-h-screen max-w-screen-2xl max-w-screen grid grid-cols-12 grid-rows-[70px_minmax(200px,_1fr)_50px]">
       <Header />
       <LocationList />
     </main>
