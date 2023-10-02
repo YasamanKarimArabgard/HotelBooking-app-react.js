efaf5fe460b9ec43a0ec8363ecb988fe9fe32619
jasminearabgard <yasaman.arabgard@outlook.com>
2023-10-03 00:43:16 +0330

    nested route for hotels

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { Route, Router, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import LocationList from './components/LocationList/LocationList'
+import AppLayout from "./components/AppLayout/AppLayout";
 
 function App() {
   return (
     <main className="app flex justify-center">
-      <main className="main-app bg-gray-50 min-h-screen max-w-screen-2xl max-w-screen grid grid-cols-12 grid-rows-[70px_minmax(200px,_1fr)_50px]">
+      <main className="main-app w-full bg-gray-50 min-h-screen max-w-screen-2xl max-w-screen grid grid-cols-12 grid-rows-[70px_minmax(200px,_1fr)_50px]">
         <Header />
         <Routes>
           <Route path="/" element={<LocationList />} />
+          <Route path="/hotels" element={<AppLayout />} >
+            <Route index element={<div>hotels</div>} />
+            <Route path=":id" element={<div>single hotels</div>} />
+          </Route>
         </Routes>
       </main>
     </main>
