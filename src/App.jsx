455fd3d74edfeb84a3991098199c0b1c4e3836bc
jasminearabgard <yasaman.arabgard@outlook.com>
2023-10-03 18:40:48 +0330

    full screen and debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import LocationList from './components/LocationList/LocationList'
 import AppLayout from "./components/AppLayout/AppLayout";
@@ -6,8 +6,8 @@ import Hotels from "./components/Hotels/Hotels";
 
 function App() {
   return (
-    <main className="app flex justify-center">
-      <main className="main-app min-w-full w-screen bg-gray-50 min-h-screen max-w-screen-2xl grid grid-cols-12 grid-rows-[70px_minmax(200px,_1fr)_20px]">
+    <main className="app-container flex justify-center bg-gray-50">
+      <main className="main-app bg-gray-50 w-full max-h-screen min-h-screen max-w-screen-2xl grid grid-cols-12 grid-rows-[70px_minmax(200px,_1fr)_20px]">
         <Header />
         <Routes>
           <Route path="/" element={<LocationList />} />
