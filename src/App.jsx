f0976007e51dff46552c51982fe9227784d10022
jasminearabgard <yasaman.arabgard@outlook.com>
2023-10-03 14:38:07 +0330

    create Hotels comp

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Router, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import LocationList from './components/LocationList/LocationList'
 import AppLayout from "./components/AppLayout/AppLayout";
+import Hotels from "./components/Hotels/Hotels";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LocationList />} />
           <Route path="/hotels" element={<AppLayout />} >
-            <Route index element={<div>hotels</div>} />
+            <Route index element={<Hotels />} />
             <Route path=":id" element={<div>single hotels</div>} />
           </Route>
         </Routes>
