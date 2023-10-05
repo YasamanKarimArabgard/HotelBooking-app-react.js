06d48e5e5713c3dc3b6911c51262b332ca05d6d9
jasminearabgard <yasaman.arabgard@outlook.com>
2023-10-05 17:06:48 +0330

    debug query on link

--- src/components/Hotels/Hotels.jsx
+++ src/components/Hotels/Hotels.jsx
@@ -12,12 +12,13 @@ const Hotels = () => {
             <h2 className="font-bold text-md">Search Result ({hotels.length})</h2>
             {
                 hotels.map(item => (
-                    <Link key={item.id} to={`hotels/${item.id}?lat=${item.latitude}?lan=${item.longitude}`}>
+                    <Link key={item.id} to={`/hotels/${item.id}?lat=${item.latitude}&lng=${item.longitude}`}>
                         <div className="flex gap-x-3 h-fit">
                             <div className="w-24">
                                 <img src={item.picture_url.url} className="w-full h-20 rounded-xl" />
                             </div>
                             <div className="flex flex-col gap-y-1">
+                            <h2 className='font-bold text-sm'>{item.smart_location}</h2>
                                 <h4 className="text-xs text-slate-400">{item.name}</h4>
                                 <div className='flex gap-x-1 items-center'>
                                     <h5 className='text-xs'>€ {item.price}</h5>
