748a6627887a8cb587be76dd2e108e21996bcf8e
jasminearabgard <yasaman.arabgard@outlook.com>
2023-10-05 17:32:25 +0330

    save lat & lng in state for backward

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,30 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useHotels } from "../Context/HotelsProvider";
-import { MapContainer, TileLayer, Marker, Popup , useMap} from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
 import { useSearchParams } from "react-router-dom";
 
 const Map = () => {
 
     const { loading, hotels } = useHotels();
-    const [mapPosition, setMapPOsition] = useState([51, 3]);
+    const [mapPosition, setMapPOsition] = useState([50, 4]);
     const [searchParams, setSearchParams] = useSearchParams();
 
-    const lat = searchParams.get('lat')
-    const lng = searchParams.get('lng')
+    const lat = searchParams.get('lat');
+    const lng = searchParams.get('lng');
+
+    useEffect(() => {
+        if (lat && lng) setMapPOsition([lat, lng])
+    }, [lat, lng]);
 
     return (
         <div className="w-3/5 rounded-tr-md rounded-br-md flex items-center justify-center">
             <MapContainer
                 className="map"
-                center={[lat || 50, lng || 3]}
+                center={mapPosition}
                 zoom={13}
-                scrollWheelZoom={false}>
+                scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy;<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
                 />
-                <ChangeCenter position={[lat || 50, lng || 3]} />
+                <ChangeCenter position={mapPosition} />
                 {
                     hotels.map(item => (
                         <Marker key={item.id} position={[item.latitude, item.longitude]}>
