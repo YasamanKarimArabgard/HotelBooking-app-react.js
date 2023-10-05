26e9b8bbcc9ae1e64dfa9267e55ba7f821d2b73b
jasminearabgard <yasaman.arabgard@outlook.com>
2023-10-05 16:32:34 +0330

    remove useless files and move to hotel provider

--- src/components/Hotels/Hotels.jsx
+++ src/components/Hotels/Hotels.jsx
@@ -1,36 +1,31 @@
-import { useSearchParams } from "react-router-dom";
-import useFetch from "../../hooks/useFetch";
+import { Link } from "react-router-dom";
+import { useHotels } from "../Context/HotelsProvider";
 
 const Hotels = () => {
 
-    const [searchParams, setSearchParams] = useSearchParams();
-    const destination = searchParams.get('destination');
-    const room = JSON.parse(searchParams.get('room'));
-
-    const { loading, data } = useFetch("http://localhost:5000/hotels",
-        `name_like=${destination || ""}&accommodates_gte=${room || ""}`);
+    const { loading, hotels } = useHotels();
 
     if (loading) return <p>loading...</p>
 
-    console.log(data);
     return (
         <div className="hotels_container flex flex-col gap-y-2">
-            <h2 className="font-bold text-md">Search Result ({data.length})</h2>
+            <h2 className="font-bold text-md">Search Result ({hotels.length})</h2>
             {
-                data.map(item => (
-                    <div key={item.id} className="flex gap-x-3 h-fit">
-                        <div className="w-24">
-                            <img src={item.picture_url.url} className="w-full h-20 rounded-xl"/>
+                hotels.map(item => (
+                    <Link key={item.id} to={`hotels/${item.id}?lat=${item.latitude}?lan=${item.longitude}`}>
+                        <div className="flex gap-x-3 h-fit">
+                            <div className="w-24">
+                                <img src={item.picture_url.url} className="w-full h-20 rounded-xl" />
+                            </div>
+                            <div className="flex flex-col gap-y-1">
+                                <h4 className="text-xs text-slate-400">{item.name}</h4>
+                                <div className='flex gap-x-1 items-center'>
+                                    <h5 className='text-xs'>€ {item.price}</h5>
+                                    <span className='text-xs text-slate-500'>night</span>
+                                </div>
+                            </div>
                         </div>
-                        <div className="flex flex-col gap-y-1">
-                            <h2 className='font-bold text-sm'>{item.smart_location}</h2>
-                            <h4 className="text-xs text-slate-400">{item.name}</h4>
-                            <div className='flex gap-x-1 items-center'> 
-                                       <h5 className='text-xs'>€ {item.price}</h5>
-                                       <span className='text-xs text-slate-500'>night</span>
-                                    </div>
-                        </div>
-                    </div>
+                    </Link>
                 ))
             }
         </div>
