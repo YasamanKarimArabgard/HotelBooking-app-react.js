5f5e7927587fa36049ede94c5a69e6e99a3472af
jasminearabgard <yasaman.arabgard@outlook.com>
2023-10-09 01:03:09 +0330

    add single hotel comp

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import AppLayout from "./components/AppLayout/AppLayout";
 import Hotels from "./components/Hotels/Hotels";
 import HotelsProvider from "./components/Context/HotelsProvider";
 import './App.css'
+import SingleHotel from "./components/SingleHotel/SingleHotel";
 
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
             <Route path="/" element={<LocationList />} />
             <Route path="/hotels" element={<AppLayout />} >
               <Route index element={<Hotels />} />
-              <Route path=":id" element={<div>single hotels</div>} />
+              <Route path=":id" element={<SingleHotel />} />
             </Route>
           </Routes>
         </HotelsProvider>
