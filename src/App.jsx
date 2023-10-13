96a7adc072808eb423d7c56765a77e5cc54ee0f6
jasminearabgard <yasaman.arabgard@outlook.com>
2023-10-13 17:29:06 +0330

    add bookmark route and provider

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,31 @@ import Hotels from "./components/Hotels/Hotels";
 import HotelsProvider from "./components/Context/HotelsProvider";
 import './App.css'
 import SingleHotel from "./components/SingleHotel/SingleHotel";
-
+import BookmarkLayout from "./components/BookmarkLayout/BookmarkLayout";
+import Bookmark from "./components/Bookmark/Bookmark"
+import BookmarkProvider from "./components/Context/BookmarkProvider";
 
 function App() {
   return (
     <main className="app-container flex justify-center bg-gray-50">
       <main className="main-app bg-gray-50 w-full max-h-screen min-h-screen max-w-screen-2xl grid grid-cols-12 grid-rows-[70px_minmax(200px,_1fr)_20px]">
-        <HotelsProvider>
-          <Header />
-          <Routes>
-            <Route path="/" element={<LocationList />} />
-            <Route path="/hotels" element={<AppLayout />} >
-              <Route index element={<Hotels />} />
-              <Route path=":id" element={<SingleHotel />} />
-            </Route>
-          </Routes>
-        </HotelsProvider>
+        <BookmarkProvider>
+          <HotelsProvider>
+            <Header />
+            <Routes>
+              <Route path="/" element={<LocationList />} />
+              <Route path="/hotels" element={<AppLayout />} >
+                <Route index element={<Hotels />} />
+                <Route path=":id" element={<SingleHotel />} />
+              </Route>
+              <Route path="/bookmarks" element={<BookmarkLayout />} >
+                <Route index element={<Bookmark />} />
+                <Route path=":id" element={<div>bookmark single</div>} />
+                <Route path="add" element={<div>book add</div>} />
+              </Route>
+            </Routes>
+          </HotelsProvider>
+        </BookmarkProvider>
       </main>
     </main>
   )
