2acb834564accc050102c7bbfb45dc073e1ab8ee
jasminearabgard <yasaman.arabgard@outlook.com>
2023-10-13 17:29:21 +0330

    redesign layout

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Map from "../Map/Map";
+import { useHotels } from "../Context/HotelsProvider";
 
 const AppLayout = () => {
+
+    const { hotels } = useHotels();
     return (
         <div className="appLayout row-start-2 col-start-2 col-span-10 border bg-white rounded-xl p-2">
             <div className="flex h-full justify-between overflow-hidden">
-                <div className="w-2/5 overflow-y-scroll">
+                <div className="w-2/6 overflow-y-scroll">
                     <Outlet />
                 </div>
-                <Map />
+                <Map markerLoactions={hotels} />
             </div>
         </div>
     );
