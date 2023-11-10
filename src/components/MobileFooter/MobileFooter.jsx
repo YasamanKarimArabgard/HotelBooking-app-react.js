f10cc927de9d767fc183c7e7fe620704bd4b76d3
jasminearabgard <yasaman.arabgard@outlook.com>
2023-11-10 23:34:18 +0330

    add active tab

--- src/components/MobileFooter/MobileFooter.jsx
+++ src/components/MobileFooter/MobileFooter.jsx
@@ -9,36 +9,33 @@ import { NavLink } from "react-router-dom";
 
 const MobileFooter = () => {
 
-
     const [activeTab, setActiveTab] = useState(1);
-
-    // console.log(activeTab);
 
     return (
         <div className="mobile-footer fixed md:hidden bottom-0 left-0 bg-white w-full bg-red shadow-2xl z-50 h-14 rounded-tl-xl rounded-tr-xl px-7 py-4">
             <ul className="flex w-full items-center justify-between">
                 {/* home icon */}
                 <li onClick={() => setActiveTab(1)}>
-                    <NavLink to={'/'} className={`cursor-pointer`}>
-                        <HomeIcon className={`w-7 h-7 text-slate-400 ${activeTab == 1 ? 'text-orange-400' : ''}`} />
+                    <NavLink to={'/'} className='cursor-pointer text-slate-400'>
+                        <HomeIcon className={`w-7 h-7 ${activeTab === 1 && 'text-orange-400'}`} />
                     </NavLink>
                 </li>
                 {/* serach icon */}
                 <li onClick={() => setActiveTab(2)}>
-                    <NavLink to={'/search'} className="cursor-pointer">
-                        <MagnifyingGlassIcon className="w-7 h-7 text-slate-400" />
+                    <NavLink to={'/search'} className="cursor-pointer text-slate-400">
+                        <MagnifyingGlassIcon className={`w-7 h-7 ${activeTab === 2 && 'text-orange-400'}`}/>
                     </NavLink>
                 </li>
                 {/* bookmark icon */}
-                <li onClick={() => setActiveTab(2)}>
-                    <NavLink to={'/bookmarks'} className="cursor-pointer">
-                        <BookmarkIcon className="w-7 h-7 text-slate-400" />
+                <li onClick={() => setActiveTab(3)}>
+                    <NavLink to={'/bookmarks'} className="cursor-pointer text-slate-400">
+                        <BookmarkIcon className={`w-7 h-7 ${activeTab === 3 && 'text-orange-400'}`}/>
                     </NavLink>
                 </li>
                 {/* profile icon */}
-                <li onClick={() => setActiveTab(2)}>
-                    <NavLink to={'/'} className="cursor-pointer">
-                        <UserCircleIcon className="w-7 h-7 text-slate-400" />
+                <li onClick={() => setActiveTab(4)}>
+                    <NavLink to={'/'} className="cursor-pointer text-slate-400">
+                        <UserCircleIcon className={`w-7 h-7 ${activeTab === 4 && 'text-orange-400'}`}/>
                     </NavLink>
                 </li>
             </ul>
