94155d466ece1b32ef76a8b35dc705f64e994c39
jasminearabgard <yasaman.arabgard@outlook.com>
2023-11-25 00:45:02 +0330

    replace class wth className

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import './Loader.css';
 const Loader = () => {
     return (
         <div className="w-full h-full flex justify-center items-center">
-           <span class="loader"></span>
+           <span className="loader"></span>
         </div>
     );
 };
