b765135998a695d6afd8ae4d6dbe1c77ee0908f1
jasminearabgard <yasaman.arabgard@outlook.com>
2023-11-25 00:43:51 +0330

    change h1 font-size

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
-import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/solid';
+import { ArrowLeftOnRectangleIcon, BookmarkIcon, } from '@heroicons/react/24/solid';
 import { useNavigate, NavLink } from 'react-router-dom';
 import { useAuth } from '../Context/AuthProvider';
 
 const Header = () => {
     return (
         <header className="header_container col-span-10 col-start-2 row-start-1">
-            <nav className='nav h-12 bg-white border border-md rounded-xl m-auto flex flex-row-reverse xl:flex-row justify-between items-center px-2 mt-2 gap-x-3'>
-                <NavLink to='/'>
-                    <h1 className='font-bold text-sm text-orange-400'>Hotel Booking App</h1>
-                </NavLink>
+            <nav className='nav h-12 bg-white border border-md rounded-xl m-auto flex flex-row-reverse justify-between items-center px-2 mt-2 gap-x-3'>
                 <User />
+                <NavLink to='/'>
+                    <h1 className='font-bold text-md md:text-xl text-orange-400 md:justify-self-center'>Hotel Booking App</h1>
+                </NavLink>
+                <NavLink to={'/bookmarks'} className="cursor-pointer text-slate-400 md:hidden">
+                    <BookmarkIcon className={`w-5 h-5 text-orange-400'}`} />
+                </NavLink>
             </nav>
         </header>
     );
@@ -32,7 +35,9 @@ function User() {
                     &nbsp; <ArrowLeftOnRectangleIcon onClick={handleLogout} className="logout w-6 h-7 text-red-500">logout</ArrowLeftOnRectangleIcon>
                 </div>
             ) : (
-                <NavLink to="/login" className='text-orange-500'>Login</NavLink>
+                <NavLink to="/login" className='text-white'>
+                    <span className='px-2 py-1 bg-orange-400 text-md rounded-lg'>Sign in</span>
+                </NavLink>
             )}
         </div>
     );
